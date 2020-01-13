3f1ed0ce8f468f4ce8e4f20da4d6b72bd7fa81d8
pradeep paul <paul.pradeep@sony.com>
2020-01-13 18:24:03 +0530

    new setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div><HomePage /></div>
+  );
+}
+
+export default App;

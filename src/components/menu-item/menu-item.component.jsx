a9386ce67fdb6ed92750dd570f35ff89b4bc8dd2
pradeep paul <paul.pradeep@sony.com>
2020-01-20 20:52:05 +0530

    collections-list

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
+
 
 const MenuItem = ({ title, imageUrl, size}) => (
     <div className={`${size} menu-item` } >
@@ -12,4 +14,4 @@ const MenuItem = ({ title, imageUrl, size}) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);

f0baf0c2806320eeed6617539df5098294f1334f
pradeep <pradeepmstech#gmail.com>
2020-01-30 22:34:53 +0530

    header started

--- src/components/collection-items/collection-item.component.jsx
+++ src/components/collection-items/collection-item.component.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
-
 const CollectionItem = ({ id, name, imageUrl, price}) => (
     <div className='collection-item'>
-        <div className='image'
-            style= {{
-                backgroundImage: `url(${imageUrl})`
-            }} />
-
+        <div className='image' style= {{ backgroundImage: `url(${imageUrl})` }} />
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
